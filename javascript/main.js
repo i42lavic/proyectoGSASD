a39d38aadb51777cdfb57cecd94726a23746a091
i42lavic <i42lavic@uco.es>
2025-11-19 11:59:06 +0100

    Update html and js

---
+++ javascript/main.js
@@ -0,0 +1,12 @@
+function recomendar(){
+    const tamano=document.getElementById("tamano").value;
+    const frecuencia=document.getElementById("frecuencia").value;
+    const colaboracion=document.getElementById("colaboracion").value;
+    const microsoft=document.getElementById("microsoft").value;
+    const ia=document.getElementById("ia").value;
+
+    let recomendacion="";
+    let explicacion="";
+
+    
+}
